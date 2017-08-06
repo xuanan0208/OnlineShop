c6f76547dde964aa066822523e77f0f63e004ebb
Xuan An <xuanan0209@gmail.com>
2017-08-06 16:51:15 +0700

    commit lan dau tien

---
+++ OnlineShop/OnlineShop/Areas/Admin/Controllers/Default1Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        //
+        // GET: /Admin/Default1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
